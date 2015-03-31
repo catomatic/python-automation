e379fd4b7939b36c08b64dc3f7103fb088033fab
catomatic <quetzlcoatl8@gmail.com>
2015-03-31 00:19:47 -0400

    Initial commit of source file

---
+++ process-cron.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+# author: catomatic
+# website: https://github.com/catomatic
+# source: personal projects library
+
+import smtplib
+import sys
+from subprocess import call, Popen, PIPE
+
+services = [
+    ('apache2', 'apache2'),
+    ('mysqld', 'mysql'),
+    ('postgres', 'postgresql'),
+    ('master', 'postfix')
+]
+
+
+class Email:
+    def __init__(self):
+        self.sender = sender
+        self.receivers = receivers
+        self.message = message
+
+    def send_email(self):
+        smtp = smtplib.SMTP('localhost')
+        smtp.sendmail(self.sender, self.receivers, self.message)
+
+
+class SendFailEmail(Email):
+    def __init__(self, *args):
+        self.sender = 'user@localhost'
+        self.receivers = ['user@localhost']
+        self.message = '''Subject: Failed Process Notice
+
+        The following processes failed to start automatically:
+        {0}
+        '''.format(args)
+
+
+class SendSuccessEmail(Email):
+    def __init__(self, *args):
+        self.sender = 'user@localhost'
+        self.receivers = ['user@localhost']
+        self.message = '''Subject: All processes running
+
+        The following processes were already running or started
+        automatically:
+        {0}
+        '''.format(args)
+
+
+class Action:
+    fail_list = []
+    success_list = []
+
+    def __init__(self):
+        self.fail_list = self.fail_list
+        self.success_list = self. success_list
+
+    def check(self):
+        for process_name, service_name in services:
+            p = Popen(['ps', '-C', process_name], stdout=PIPE)
+            output, error = p.communicate()
+            if process_name not in output:
+                self.fail_list.append(service_name)
+            if process_name in output:
+                self.success_list.append(service_name)
+
+
+class Start(Action):
+
+    def __init__(self):
+        Action.__init__(self)
+
+    def do_action(self):
+        if len(self.fail_list) > 0:
+            for service in self.fail_list:
+                call(['sudo', 'service', service, 'start'])
+
+
+class Notify(Action):
+
+    def __init__(self):
+        Action.__init__(self)
+
+    def do_action(self):
+        if len(self.fail_list) > 0:
+            SendFailEmail(self.fail_list).send_email()
+        else:
+            SendSuccessEmail(self.success_list).send_email()
+
+
+def main():
+    if sys.argv[1] == 'start':
+        Start().check()
+        Start().do_action()
+    elif sys.argv[1] == 'notify':
+        Notify().check()
+        Notify().do_action()
+
+
+if __name__ == '__main__':
+    main()
